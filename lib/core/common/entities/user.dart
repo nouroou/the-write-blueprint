8843014c4f86640534012d98b64e1102a4b6d28f
Noureddine Kechroud <nouroukechroud@icloud.com>
2025-05-02 15:41:16 +0100

    Main branch: Core. Add user entity that is used globally in the app's features.

---
+++ lib/core/common/entities/user.dart
@@ -0,0 +1,10 @@
+class User {
+  final String id, email, name, username, avatarUrl;
+
+  User(
+      {required this.id,
+      required this.email,
+      required this.name,
+      required this.username,
+      required this.avatarUrl});
+}
